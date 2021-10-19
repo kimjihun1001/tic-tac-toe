55f2f0bbbdd1421ebfd7fa47d8edb692f7510de0
kimjihoon <kimjihun1001@gmail.com>
2021-10-19 15:02:30 +0900

    211019

--- TicTacToe/tictactoe.cs
+++ TicTacToe/tictactoe.cs
@@ -23,111 +23,13 @@ namespace TicTacToe
             }
         }
 
-        // 모양 그리기 함수
-        public static void PaintShape(string shape, string input)
+        // 게임 실행 함수
+        public static void PrintFirstMenuScreen()
         {
-            switch (input)
-            {
-                case "1":
-                    WriteAt(shape, 2, 1);
-                    break;
-                case "2":
-                    WriteAt(shape, 6, 1);
-                    break;
-                case "3":
-                    WriteAt(shape, 10, 1);
-                    break;
-                case "4":
-                    WriteAt(shape, 2, 3);
-                    break;
-                case "5":
-                    WriteAt(shape, 6, 3);
-                    break;
-                case "6":
-                    WriteAt(shape, 10, 3);
-                    break;
-                case "7":
-                    WriteAt(shape, 2, 5);
-                    break;
-                case "8":
-                    WriteAt(shape, 6, 5);
-                    break;
-                case "9":
-                    WriteAt(shape, 10, 5);
-                    break;
-                default:
-                    Console.WriteLine("1~9 사이의 번호로 다시 입력하세요:");
-                    PaintShape(shape, input);
-                    break;
-            }
-            WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
-            Console.SetCursorPosition(0, 10);   // 순서 안내문 위치로 커서 이동 
-        }
- 
-        static void Main(string[] args)
-        {
-            // Clear the screen, then save the top and left coordinates.
             Console.Clear();
-            origRow = Console.CursorTop;
-            origCol = Console.CursorLeft;
+            PaintTable();
+            Console.WriteLine("게임을 시작합니다");
 
-            // 칸 숫자 그리기
-            WriteAt("1", 0, 0);
-            WriteAt("2", 4, 0);
-            WriteAt("3", 8, 0);
-            WriteAt("4", 0, 2);
-            WriteAt("5", 4, 2);
-            WriteAt("6", 8, 2);
-            WriteAt("7", 0, 4);
-            WriteAt("8", 4, 4);
-            WriteAt("9", 8, 4);
-
- 
-            // 가로선 그리기
-            WriteAt("---", 1, 0);
-            WriteAt("---", 5, 0);
-            WriteAt("---", 9, 0);
-            WriteAt("---", 1, 2);
-            WriteAt("---", 5, 2);
-            WriteAt("---", 9, 2);
-            WriteAt("---", 1, 4);
-            WriteAt("---", 5, 4);
-            WriteAt("---", 9, 4);
-            WriteAt("---", 1, 6);
-            WriteAt("---", 5, 6);
-            WriteAt("---", 9, 6);
-
-            // 세로선 그리기
-            WriteAt("|", 0, 1);
-            WriteAt("|", 4, 1);
-            WriteAt("|", 8, 1);
-            WriteAt("|", 12, 1);
-            WriteAt("|", 0, 3);
-            WriteAt("|", 4, 3);
-            WriteAt("|", 8, 3);
-            WriteAt("|", 12, 3);
-            WriteAt("|", 0, 5);
-            WriteAt("|", 4, 5);
-            WriteAt("|", 8, 5);
-            WriteAt("|", 12, 5);
-
-            // 꼭짓점 그리기
-            WriteAt("+", 12, 0);
-            WriteAt("+", 12, 2);
-            WriteAt("+", 12, 4);
-            WriteAt("+", 12, 6);
-            WriteAt("+", 0, 6);
-            WriteAt("+", 4, 6);
-            WriteAt("+", 8, 6);
-
-
-            WriteAt("게임을 시작합니다", 0, 9);
-            Console.WriteLine();    // 방금 커서가 있던 곳에서 다음줄로 넘어가게 하는 듯. 이걸 안해주면 아래처럼 커서 포지션 다시 정해야 함.
-
-            // Console.SetCursorPosition(0, 6);
-
-
-            // 다시 천천히 만들어보자.
             string shape;
 
             string one = null;
@@ -142,7 +44,7 @@ namespace TicTacToe
 
             bool isThereWinner = false;
 
-            for (int i=0; i < 9; i++)
+            for (int i = 0; i < 9; i++)
             {
                 if (i % 2 == 0)
                 {
@@ -153,13 +55,13 @@ namespace TicTacToe
                     shape = "X";
                 }
 
-                Console.WriteLine(shape + "차례입니다. 원하는 칸의 번호를 입력하세요: ");
+                Console.WriteLine(shape + " 차례입니다. 원하는 칸의 번호를 입력하세요: ");
 
                 bool isInputWrong = true;
-                while(isInputWrong)
+                while (isInputWrong)
                 {
                     string input = Console.ReadLine();
-                    
+
                     Console.SetCursorPosition(0, 10);   // 순서 안내문 위치로 이동 
 
                     switch (input)
@@ -300,7 +202,8 @@ namespace TicTacToe
                             }
                             break;
                         default:
-                            Console.WriteLine("1~9 사이의 번호로 다시 입력하세요:                 ");
+                            // int.Parse()와 달리 이렇게 switch 문으로 작성하면 "문자 입력 예외 처리" 가ㄴ
+                            Console.WriteLine("1~9 사이의 숫채로 다시 입력하세요:                 ");
                             WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
                             Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
                             input = null;
@@ -372,7 +275,146 @@ namespace TicTacToe
             {
 
             }
+
+
+        }
+
+
+        // 3번 메뉴 화면 출력
+        public static void PrintThirdMenuScreen()
+        {
+            Console.WriteLine("TTTTT   I    CCC   TTTTT     A     CCC   TTTTT   OO    EEEEE");
+            Console.WriteLine("  T     I   CC       T      A A   CC       T    O  O   E    ");
+            Console.WriteLine("  T     I    CCC     T     A   A   CCC     T     OO    EEEEE");
+            Console.WriteLine("============================================================");
+            Console.WriteLine("Tic-tac-toe (American English), noughts and crosses (Commonwe\n" +
+                              "alth English), or Xs and Os (Irish English) is a paper-and-pen\n" +
+                              "cil game for two players who take turns marking the spaces in a three-by-three grid with X or O. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. It is a solved game, with a forced draw assuming best play from both players.");
+        }
+
+        // 칸 그리기 함수
+        public static void PaintTable()
+        {
+            Console.WriteLine("1---2---3---+");
+            Console.WriteLine("|   |   |   |");
+            Console.WriteLine("4---5---6---+");
+            Console.WriteLine("|   |   |   |");
+            Console.WriteLine("7---8---9---+");
+            Console.WriteLine("|   |   |   |");
+            Console.WriteLine("+---+---+---+");
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        // 모양 그리기 함수
+        public static void PaintShape(string shape, string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    WriteAt(shape, 2, 1);
+                    break;
+                case "2":
+                    WriteAt(shape, 6, 1);
+                    break;
+                case "3":
+                    WriteAt(shape, 10, 1);
+                    break;
+                case "4":
+                    WriteAt(shape, 2, 3);
+                    break;
+                case "5":
+                    WriteAt(shape, 6, 3);
+                    break;
+                case "6":
+                    WriteAt(shape, 10, 3);
+                    break;
+                case "7":
+                    WriteAt(shape, 2, 5);
+                    break;
+                case "8":
+                    WriteAt(shape, 6, 5);
+                    break;
+                case "9":
+                    WriteAt(shape, 10, 5);
+                    break;
+                default:
+                    Console.WriteLine("1~9 사이의 번호로 다시 입력하세요:");
+                    PaintShape(shape, input);
+                    break;
+            }
+            WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+            Console.SetCursorPosition(0, 10);   // 순서 안내문 위치로 커서 이동 
+        }
+
+        public static void PrintSecondMenuScreen()
+        {
+
+        }
+
+
+        // 메인 선택 화면 출력 
+        public static void PrintMainScreen()
+        {
+            // 메인 선택 화면
+            Console.Clear();
             
+            Console.WriteLine("TTTTT   I    CCC   TTTTT     A     CCC   TTTTT   OO    EEEEE");
+            Console.WriteLine("  T     I   CC       T      A A   CC       T    O  O   E    ");
+            Console.WriteLine("  T     I    CCC     T     A   A   CCC     T     OO    EEEEE");
+            Console.WriteLine("============================================================");
+            Console.WriteLine("메뉴를 선택해주세요.");
+            Console.WriteLine("1: 플레이어 대 플레이어");
+            Console.WriteLine("2: 플레이어 대 컴퓨터");
+            Console.WriteLine("3: 게임 설명");
+            string mode = Console.ReadLine();
+
+            // 메뉴 선택에 따라 
+            try
+            {
+                switch (mode)
+                {
+                    case "1":
+                        PrintFirstMenuScreen();
+                        break;
+
+                    case "2":
+                        PrintSecondMenuScreen();
+                        break;
+
+                    case "3":
+                        PrintThirdMenuScreen();
+                        break;
+
+                    default:
+                        PrintMainScreen();
+                        break;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Clear();
+                Console.WriteLine(e.Message);
+                PrintMainScreen();
+            }
+        }
+ 
+        static void Main(string[] args)
+        {
+            
+            // Clear the screen, then save the top and left coordinates.
+            Console.Clear();
+            origRow = Console.CursorTop;
+            origCol = Console.CursorLeft;
+
+            PrintMainScreen();
+
+            // Console.WriteLine();
+            // 방금 커서가 있던 곳에서 다음줄로 넘어가게 하는 듯. 이걸 안해주면 아래처럼 커서 포지션 다시 정해야 함.
+            // Console.SetCursorPosition(0, 6);
+
+
+
 
         }
     }
