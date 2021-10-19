177b485ecf3868037b9ddb1464c912a4aa391cf6
kimjihoon <kimjihun1001@gmail.com>
2021-10-19 09:26:00 +0900

    211019

--- TicTacToe/tictactoe.cs
+++ TicTacToe/tictactoe.cs
@@ -18,52 +18,52 @@ namespace TicTacToe
             }
             catch (ArgumentOutOfRangeException e)
             {
-                //Console.Clear();
+                Console.Clear();
                 Console.WriteLine(e.Message);
             }
         }
 
         // 모양 그리기 함수
-        public static void PaintShape(string shape)
+        public static void PaintShape(string shape, string input)
         {
-            for (int i = 0; i < 9; i++)
+            switch (input)
             {
-                string input = Console.ReadLine();
-                switch (input)
-                {
-                    case "1":
-                        WriteAt(shape, 2, 1);
-                        break;
-                    case "2":
-                        WriteAt(shape, 6, 1);
-                        break;
-                    case "3":
-                        WriteAt(shape, 10, 1);
-                        break;
-                    case "4":
-                        WriteAt(shape, 2, 3);
-                        break;
-                    case "5":
-                        WriteAt(shape, 6, 3);
-                        break;
-                    case "6":
-                        WriteAt(shape, 10, 3);
-                        break;
-                    case "7":
-                        WriteAt(shape, 2, 5);
-                        break;
-                    case "8":
-                        WriteAt(shape, 6, 5);
-                        break;
-                    case "9":
-                        WriteAt(shape, 10, 5);
-                        break;
-                }
-                WriteAt(" ", 0, 10);
-                Console.SetCursorPosition(0, 10);
+                case "1":
+                    WriteAt(shape, 2, 1);
+                    break;
+                case "2":
+                    WriteAt(shape, 6, 1);
+                    break;
+                case "3":
+                    WriteAt(shape, 10, 1);
+                    break;
+                case "4":
+                    WriteAt(shape, 2, 3);
+                    break;
+                case "5":
+                    WriteAt(shape, 6, 3);
+                    break;
+                case "6":
+                    WriteAt(shape, 10, 3);
+                    break;
+                case "7":
+                    WriteAt(shape, 2, 5);
+                    break;
+                case "8":
+                    WriteAt(shape, 6, 5);
+                    break;
+                case "9":
+                    WriteAt(shape, 10, 5);
+                    break;
+                default:
+                    Console.WriteLine("1~9 사이의 번호로 다시 입력하세요:");
+                    PaintShape(shape, input);
+                    break;
             }
+            WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+            Console.SetCursorPosition(0, 10);   // 순서 안내문 위치로 커서 이동 
         }
-
+ 
         static void Main(string[] args)
         {
             // Clear the screen, then save the top and left coordinates.
@@ -121,13 +121,258 @@ namespace TicTacToe
             WriteAt("+", 8, 6);
 
 
-            WriteAt("Done", 0, 9);
+            WriteAt("게임을 시작합니다", 0, 9);
             Console.WriteLine();    // 방금 커서가 있던 곳에서 다음줄로 넘어가게 하는 듯. 이걸 안해주면 아래처럼 커서 포지션 다시 정해야 함.
 
             // Console.SetCursorPosition(0, 6);
-            PaintShape("O");
 
 
+            // 다시 천천히 만들어보자.
+            string shape;
+
+            string one = null;
+            string two = null;
+            string three = null;
+            string four = null;
+            string five = null;
+            string six = null;
+            string seven = null;
+            string eight = null;
+            string nine = null;
+
+            bool isThereWinner = false;
+
+            for (int i=0; i < 9; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    shape = "O";
+                }
+                else
+                {
+                    shape = "X";
+                }
+
+                Console.WriteLine(shape + "차례입니다. 원하는 칸의 번호를 입력하세요: ");
+
+                bool isInputWrong = true;
+                while(isInputWrong)
+                {
+                    string input = Console.ReadLine();
+                    
+                    Console.SetCursorPosition(0, 10);   // 순서 안내문 위치로 이동 
+
+                    switch (input)
+                    {
+                        case "1":
+                            if (one != null)
+                            {
+                                Console.WriteLine("이미 입력된 칸입니다. 다시 입력하세요:            ");
+                                WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+                                Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
+                                input = null;
+                            }
+                            else
+                            {
+                                PaintShape(shape, input);
+                                one = shape;
+                                isInputWrong = false;
+                            }
+                            break;
+                        case "2":
+                            if (two != null)
+                            {
+                                Console.WriteLine("이미 입력된 칸입니다. 다시 입력하세요:            ");
+                                WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+                                Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
+                                input = null;
+                            }
+                            else
+                            {
+                                PaintShape(shape, input);
+                                two = shape;
+                                isInputWrong = false;
+                            }
+                            break;
+                        case "3":
+                            if (three != null)
+                            {
+                                Console.WriteLine("이미 입력된 칸입니다. 다시 입력하세요:            ");
+                                WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+                                Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
+                                input = null;
+                            }
+                            else
+                            {
+                                PaintShape(shape, input);
+                                three = shape;
+                                isInputWrong = false;
+                            }
+                            break;
+                        case "4":
+                            if (four != null)
+                            {
+                                Console.WriteLine("이미 입력된 칸입니다. 다시 입력하세요:            ");
+                                WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+                                Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
+                                input = null;
+                            }
+                            else
+                            {
+                                PaintShape(shape, input);
+                                four = shape;
+                                isInputWrong = false;
+                            }
+                            break;
+                        case "5":
+                            if (five != null)
+                            {
+                                Console.WriteLine("이미 입력된 칸입니다. 다시 입력하세요:            ");
+                                WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+                                Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
+                                input = null;
+                            }
+                            else
+                            {
+                                PaintShape(shape, input);
+                                five = shape;
+                                isInputWrong = false;
+                            }
+                            break;
+                        case "6":
+                            if (six != null)
+                            {
+                                Console.WriteLine("이미 입력된 칸입니다. 다시 입력하세요:            ");
+                                WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+                                Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
+                                input = null;
+                            }
+                            else
+                            {
+                                PaintShape(shape, input);
+                                six = shape;
+                                isInputWrong = false;
+                            }
+                            break;
+                        case "7":
+                            if (seven != null)
+                            {
+                                Console.WriteLine("이미 입력된 칸입니다. 다시 입력하세요:            ");
+                                WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+                                Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
+                                input = null;
+                            }
+                            else
+                            {
+                                PaintShape(shape, input);
+                                seven = shape;
+                                isInputWrong = false;
+                            }
+                            break;
+                        case "8":
+                            if (eight != null)
+                            {
+                                Console.WriteLine("이미 입력된 칸입니다. 다시 입력하세요:            ");
+                                WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+                                Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
+                                input = null;
+                            }
+                            else
+                            {
+                                PaintShape(shape, input);
+                                eight = shape;
+                                isInputWrong = false;
+                            }
+                            break;
+                        case "9":
+                            if (nine != null)
+                            {
+                                Console.WriteLine("이미 입력된 칸입니다. 다시 입력하세요:            ");
+                                WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+                                Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
+                                input = null;
+                            }
+                            else
+                            {
+                                PaintShape(shape, input);
+                                nine = shape;
+                                isInputWrong = false;
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("1~9 사이의 번호로 다시 입력하세요:                 ");
+                            WriteAt("  ", 0, 11);   // 숫자 입력받는 칸 지우기
+                            Console.SetCursorPosition(0, 11);   // 숫자 입력받는 칸으로 커서 이동
+                            input = null;
+                            break;
+                    }
+
+                }
+
+                if (one == shape && two == shape && three == shape)
+                {
+                    Console.WriteLine(shape + "의 승리입니다.                              ");
+                    isThereWinner = true;
+                    break;
+                }
+                else if (four == shape && five == shape && six == shape)
+                {
+                    Console.WriteLine(shape + "의 승리입니다.                              ");
+                    isThereWinner = true;
+                    break;
+                }
+                else if (seven == shape && eight == shape && nine == shape)
+                {
+                    Console.WriteLine(shape + "의 승리입니다.                              ");
+                    isThereWinner = true;
+                    break;
+                }
+                else if (one == shape && four == shape && seven == shape)
+                {
+                    Console.WriteLine(shape + "의 승리입니다.                              ");
+                    isThereWinner = true;
+                    break;
+                }
+                else if (two == shape && five == shape && eight == shape)
+                {
+                    Console.WriteLine(shape + "의 승리입니다.                              ");
+                    isThereWinner = true;
+                    break;
+                }
+                else if (three == shape && six == shape && nine == shape)
+                {
+                    Console.WriteLine(shape + "의 승리입니다.                              ");
+                    isThereWinner = true;
+                    break;
+                }
+                else if (one == shape && five == shape && nine == shape)
+                {
+                    Console.WriteLine(shape + "의 승리입니다.                              ");
+                    isThereWinner = true;
+                    break;
+                }
+                else if (three == shape && five == shape && seven == shape)
+                {
+                    Console.WriteLine(shape + "의 승리입니다.                              ");
+                    isThereWinner = true;
+                    break;
+                }
+                else
+                {
+
+                }
+
+            }
+
+            if (isThereWinner == false)
+            {
+                Console.WriteLine("무승부입니다.                              \n다음 게임을 시작하시겠습니까?");
+            }
+            else
+            {
+
+            }
+            
 
         }
     }
